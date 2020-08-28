f26d90d3ea9a829fb997f5e3c738bd67becf048f
Philip Zubel <philip.zubel1@gmail.com>
2020-08-28 19:00:09 +0200

    Updates

---
+++ precache-manifest.1c1d7b97b5627d3ad0c2f032efc95798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a40ff64f08e96879bf2",
+    "url": "/currencyConverter/static/css/main.f7480111.chunk.css"
+  },
+  {
+    "revision": "9a40ff64f08e96879bf2",
+    "url": "/currencyConverter/static/js/main.5c1e9137.chunk.js"
+  },
+  {
+    "revision": "d7e124b8bc986263b5cd",
+    "url": "/currencyConverter/static/js/runtime~main.133cfc32.js"
+  },
+  {
+    "revision": "3d370f07b04eeb79816a",
+    "url": "/currencyConverter/static/css/2.3bf97dea.chunk.css"
+  },
+  {
+    "revision": "3d370f07b04eeb79816a",
+    "url": "/currencyConverter/static/js/2.98a8bec1.chunk.js"
+  },
+  {
+    "revision": "4f1ad4708c98c3fd2f0385ef2adae38e",
+    "url": "/currencyConverter/index.html"
+  }
+];
